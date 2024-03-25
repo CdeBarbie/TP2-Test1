87a20f877eec6e4d39df8ce243ac159106dc19dd
nofem <tradingfamy@gmail.com>
2024-03-25 12:07:57 +0200

    New register_party commit

--- register_party.py
+++ register_party.py
@@ -1,3 +1,9 @@
+"""2.1 Write a python function named “register_party” that takes a list of parties. Each party
+must be presented by key value pairs. The keys should be “party_name”, “reg_number”,
+“member_count”). The function should check if the new party has acceptable number of
+members for it to be registered as per the requirements narrated in the scenario. Note: this
+function must be written under the git branch that you created in question 1.4 above. ("""
+
 party_name =  ""
 member_count = 0
 reg_number = 0
@@ -18,3 +24,4 @@ def register_party():
             print("This party ", party_name, "is not registered with a number of ", member_count, "due to a minimum of members")
 
 register_party()
+print(Parties)
